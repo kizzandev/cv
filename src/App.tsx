f6613af9a8c414c44f36738692d32ca110bab6d4
kizzandev <69643444+kizzandev@users.noreply.github.com>
2024-05-19 12:27:53 -0300

    Add max width

--- src/App.tsx
+++ src/App.tsx
@@ -14,14 +14,15 @@ function App() {
       min-h-screen
       text-[var(--color-dark)] dark:text-[var(--color-light)]
       py-[50px] px-[16px]
-      transition-colors duration-300
-      flex flex-col gap-[64px]"
+      transition-colors duration-300"
     >
-      <Header lang={lang} setLang={setLang} />
-      <About lang={lang} />
-      <Bento lang={lang} />
-      <Experience lang={lang} />
-      <Technologies lang={lang} />
+      <div className="flex flex-col gap-[64px] max-w-7xl mx-auto">
+        <Header lang={lang} setLang={setLang} />
+        <About lang={lang} />
+        <Bento lang={lang} />
+        <Experience lang={lang} />
+        <Technologies lang={lang} />
+      </div>
     </div>
   );
 }
