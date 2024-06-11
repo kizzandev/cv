1238216ab3b57f045f8a3091d7dcf49482804de4
kizzandev <69643444+kizzandev@users.noreply.github.com>
2024-06-11 12:46:42 -0300

    Fix: Bento

--- src/components/bento.tsx
+++ src/components/bento.tsx
@@ -20,7 +20,7 @@ import { languageContext } from "../Root";
 const data_ar = data.ar.bento;
 const data_en = data.en.bento;
 
-export default function Bento() {
+export default function BentoWIP() {
   // We only want title and description from the RSS feed
   // item[0].title and item[0].description
   // Get RSS feed from
@@ -81,18 +81,22 @@ export default function Bento() {
     <article className="flex flex-col gap-4">
       {/* MARK: TOP BENTO
        */}
+      {/* set all widts to the same */}
       <section
-        id="topBento"
-        className="grid grid-cols-6 grid-rows-2 gap-4
+        id="bento"
+        className="grid grid-cols-[18] grid-rows-4 gap-4
+         auto-cols-fr
       [&>article]:bg-[var(--bg-secondary)]
       [&>article]:p-[16px]
-      [&>article]:rounded-[var(--border-radius-8)]"
+      [&>article]:rounded-[var(--border-radius-8)]
+      "
       >
         {/* MARK: CURRENT JOB
          */}
         <article
           className="flex flex-col gap-1
-          col-span-full row-span-2 md:col-span-4 _md:col-span-2
+          col-span-full md:col-[1/span_12] _md:col-[1/span_6]
+          row-[1/span_2]
            dark:bg-gradient-to-tr dark:from-[var(--bg-primary-dark)] dark:to-[var(--bg-secondary)]"
         >
           <h2
@@ -113,7 +117,8 @@ export default function Bento() {
          */}
         <article
           className="hidden _flex flex-col gap-1
-          col-span-full row-span-2 md:col-span-2
+          col-span-full md:col-[7/span_6]
+          row-[1/span_2]
           dark:bg-gradient-to-tr dark:from-[var(--bg-primary-dark)] dark:to-[var(--bg-secondary)]"
         >
           <h2
@@ -134,182 +139,165 @@ export default function Bento() {
           </p>
         </article>
 
-        {/* MARK: MY DATA
+        {/* MARK: YEARS OF EXPERIENCE
          */}
         <article
           className="
-         !bg-transparent
-         !p-0
-          grid grid-cols-1 lg:grid-cols-2 gap-4
-         col-span-full row-span-2
-        md:col-span-2
-        [&>section]:bg-[var(--bg-secondary)]
-        [&>section]:p-[16px]
-        [&>section]:rounded-[var(--border-radius-8)]
-        "
+           dark:bg-gradient-to-tr dark:from-[var(--bg-primary-dark)] dark:to-[var(--bg-secondary)]
+          col-[13/span_3] row-[1/span_1]"
         >
-          {/* MARK: YEARS OF EXPERIENCE
-           */}
-          <section
-            className="
-           dark:bg-gradient-to-tr dark:from-[var(--bg-primary-dark)] dark:to-[var(--bg-secondary)]"
-          >
-            <h2
-              className="text-[calc(var(--h3))]
+          <h2
+            className="text-[calc(var(--h3))]
               text-[var(--color-accent-light)] dark:text-[var(--color-accent-dark)]"
-            >
-              {data.common.years_of_experience}+
-            </h2>
-            <p className="text-[calc(var(--p))] leading-6">
-              {lang === "es" ? data_ar.years : data_en.years}
-            </p>
-          </section>
-
-          {/* MARK: CURRENT JOB
-           */}
-          <section
-            className="hidden lg:block 
-           dark:bg-gradient-to-tr dark:from-[var(--bg-primary-dark)] dark:to-[var(--bg-secondary)]"
           >
-            <h2
-              className="text-[calc(var(--h3))]
-              text-[var(--color-accent-light)] dark:text-[var(--color-accent-dark)]"
-            >
-              {/* {data_ar.now} */}
-              {lang === "es" ? data_ar.now : data_en.now}
-            </h2>
-            <p className="text-[calc(var(--p))] leading-6">
-              {lang === "es" ? data_ar.job_title : data_en.job_title}
-            </p>
-          </section>
-
-          {/* MARK: EDUCATION
-           */}
-          <section
-            className="
-           dark:bg-gradient-to-tr dark:from-[var(--bg-primary-dark)] dark:to-[var(--bg-secondary)]"
-          >
-            <h2
-              className="text-[clamp(calc(var(--h3-low)),_calc(var(--h3)),_100%)]
-              text-[var(--color-accent-light)] dark:text-[var(--color-accent-dark)]"
-            >
-              {lang === "es" ? data_ar.education : data_en.education}
-            </h2>
-            <p className="text-[calc(var(--p))] leading-6">
-              {lang === "es" ? data_ar.degree : data_en.degree}
-            </p>
-          </section>
-
-          {/* MARK: LANGUAGES
-           */}
-          <section
-            className="
-           dark:bg-gradient-to-tr dark:from-[var(--bg-primary-dark)] dark:to-[var(--bg-secondary)]"
-          >
-            <article className="flex gap-4 flex-wrap">
-              <h2
-                className="w-full h-auto text-[clamp(calc(var(--h3-low)),_calc(var(--h3)),_100%)]
-              text-[var(--color-accent-light)] dark:text-[var(--color-accent-dark)]"
-              >
-                {lang === "es" ? data_ar.languages : data_en.languages}
-              </h2>
-              {data.common.langs.map((lang, idx) => (
-                <img
-                  key={`${idx}lang`}
-                  src={`/${lang}_flag.webp`}
-                  alt={`${flagAlt} ${
-                    lang === "ar" ? "Argentina" : "United States"
-                  }`}
-                  className="rounded-[4px]
-                  max-w-[76px] max-h-[42.75px] aspect-[76/42.75]"
-                />
-              ))}
-            </article>
-            {/* <p className="text-[calc(var(--p))] leading-6 mt-3">
-              {lang === "es" ? data_ar.languages : data_en.languages}
-            </p> */}
-          </section>
+            {data.common.years_of_experience}+
+          </h2>
+          <p className="text-[calc(var(--p))] leading-6">
+            {lang === "es" ? data_ar.years : data_en.years}
+          </p>
         </article>
-      </section>
 
-      {/* MARK: BOTTOM BENTO
-       */}
-      <section
-        id="bottomBento"
-        className="grid grid-cols-6 grid-rows-2 gap-4
-      [&>article]:bg-[var(--bg-secondary)]
-      [&>article]:p-[16px]
-      [&>article]:rounded-[var(--border-radius-8)]"
-      >
+        {/* MARK: CURRENT JOB
+         */}
+        <article
+          className="hidden lg:block 
+           dark:bg-gradient-to-tr dark:from-[var(--bg-primary-dark)] dark:to-[var(--bg-secondary)]
+           col-[16/span_3] row-[1/span_1]"
+        >
+          <h2
+            className="text-[calc(var(--h3))]
+              text-[var(--color-accent-light)] dark:text-[var(--color-accent-dark)]"
+          >
+            {/* {data_ar.now} */}
+            {lang === "es" ? data_ar.now : data_en.now}
+          </h2>
+          <p className="text-[calc(var(--p))] leading-6">
+            {lang === "es" ? data_ar.job_title : data_en.job_title}
+          </p>
+        </article>
+
+        {/* MARK: EDUCATION
+         */}
         <article
           className="
-         !bg-transparent
-         !p-0
-          grid gap-4
-         col-span-full row-span-2
-        md:col-span-4
-        [&>section>article]:bg-[var(--bg-secondary)]
-        [&>section>article]:p-[16px]
-        [&>section>article]:rounded-[var(--border-radius-8)]
-        "
+           dark:bg-gradient-to-tr dark:from-[var(--bg-primary-dark)] dark:to-[var(--bg-secondary)]
+           col-[13/span_3] row-[2/span_1]"
         >
-          <section className="grid gap-4 grid-cols-1 md:grid-cols-3 col-span-full [&>article]:md:col-span-1">
-            <article className="flex items-center min-h-[100px] relative overflow-hidden">
-              <IconNode className="left-0 absolute w-full max-w-[50%] h-full z-10" />
-              <div className="left-0 bg-gradient-to-t from-black to-transparent w-full h-full absolute z-20"></div>
-              <p className="text-[calc(var(--p))] leading-6 text-[var(--color-accent-dark)] font-semibold mt-auto z-40">
-                {lang === "es" ? data_ar.list_1_1 : data_en.list_1_1}
-              </p>
-            </article>
+          <h2
+            className="text-[clamp(calc(var(--h3-low)),_calc(var(--h3)),_100%)]
+              text-[var(--color-accent-light)] dark:text-[var(--color-accent-dark)]"
+          >
+            {lang === "es" ? data_ar.education : data_en.education}
+          </h2>
+          <p className="text-[calc(var(--p))] leading-6">
+            {lang === "es" ? data_ar.degree : data_en.degree}
+          </p>
+        </article>
 
-            <article className="flex items-center min-h-[100px] relative overflow-hidden">
-              <IconNext className="left-0 absolute w-full max-w-[50%] h-full z-10" />
-              <div className="left-0 bg-gradient-to-t from-black to-transparent w-full h-full absolute z-20"></div>
-              <p className="text-[calc(var(--p))] leading-6 text-[var(--color-accent-dark)] font-semibold mt-auto z-40">
-                {lang === "es" ? data_ar.list_1_2 : data_en.list_1_2}
-              </p>
-            </article>
-
-            <article className="flex items-center min-h-[100px] relative overflow-hidden">
-              <IconReact className="left-0 absolute w-full max-w-[50%] h-full z-10" />
-              <div className="left-0 bg-gradient-to-t from-black to-transparent w-full h-full absolute z-20"></div>
-              <p className="text-[calc(var(--p))] leading-6 text-[var(--color-accent-dark)] font-semibold mt-auto z-40">
-                {lang === "es" ? data_ar.list_1_3 : data_en.list_1_3}
-              </p>
-            </article>
+        {/* MARK: LANGUAGES
+         */}
+        <article
+          className="
+           dark:bg-gradient-to-tr dark:from-[var(--bg-primary-dark)] dark:to-[var(--bg-secondary)]
+        col-[16/span_3] row-[2/span_1]"
+        >
+          <section className="flex gap-4 flex-wrap">
+            <h2
+              className="w-full h-auto text-[clamp(calc(var(--h3-low)),_calc(var(--h3)),_100%)]
+              text-[var(--color-accent-light)] dark:text-[var(--color-accent-dark)]"
+            >
+              {lang === "es" ? data_ar.languages : data_en.languages}
+            </h2>
+            {data.common.langs.map((lang, idx) => (
+              <img
+                key={`${idx}lang`}
+                src={`/${lang}_flag.webp`}
+                alt={`${flagAlt} ${
+                  lang === "ar" ? "Argentina" : "United States"
+                }`}
+                className="rounded-[4px]
+                  max-w-[76px] max-h-[42.75px] aspect-[76/42.75]"
+              />
+            ))}
           </section>
+          {/* <p className="text-[calc(var(--p))] leading-6 mt-3">
+              {lang === "es" ? data_ar.languages : data_en.languages}
+            </p> */}
+        </article>
 
-          <section className="grid gap-4 grid-cols-1 md:grid-cols-3 col-span-full [&>article]:md:col-span-1">
-            <article className="flex items-center min-h-[100px] relative overflow-hidden">
-              <IconProblems className="left-0 absolute w-full max-w-[50%] h-full z-10" />
-              <div className="left-0 bg-gradient-to-t from-black to-transparent w-full h-full absolute z-20"></div>
-              <p className="text-[calc(var(--p))] leading-6 text-[var(--color-accent-dark)] font-semibold mt-auto z-40">
-                {lang === "es" ? data_ar.list_2_1 : data_en.list_2_1}
-              </p>
-            </article>
-
-            <article className="flex items-center min-h-[100px] relative overflow-hidden">
-              <IconTeam className="left-0 absolute w-full max-w-[50%] h-full z-10" />
-              <div className="left-0 bg-gradient-to-t from-black to-transparent w-full h-full absolute z-20"></div>
-              <p className="text-[calc(var(--p))] leading-6 text-[var(--color-accent-dark)] font-semibold mt-auto z-40">
-                {lang === "es" ? data_ar.list_2_2 : data_en.list_2_2}
-              </p>
-            </article>
-
-            <article className="flex items-center min-h-[100px] relative overflow-hidden">
-              <IconAuthor className="left-0 absolute w-full max-w-[50%] h-full z-10" />
-              <div className="left-0 bg-gradient-to-t from-black to-transparent w-full h-full absolute z-20"></div>
-              <p className="text-[calc(var(--p))] leading-6 text-[var(--color-accent-dark)] font-semibold mt-auto z-40">
-                {lang === "es" ? data_ar.list_2_3 : data_en.list_2_3}
-              </p>
-            </article>
-          </section>
+        {/* MARK: TOOLS
+         */}
+        <article
+          className="flex items-center min-h-[100px] relative overflow-hidden
+        col-span-full md:col-[1/span_4] row-[3/span_1]"
+        >
+          <IconNode className="left-0 absolute w-full max-w-[50%] h-full z-10" />
+          <div className="left-0 bg-gradient-to-t from-black to-transparent w-full h-full absolute z-20"></div>
+          <p className="text-[calc(var(--p))] leading-6 text-[var(--color-accent-dark)] font-semibold mt-auto z-40">
+            {lang === "es" ? data_ar.list_1_1 : data_en.list_1_1}
+          </p>
         </article>
 
         <article
-          className="grid gap-1
-         col-span-full row-span-2
-        md:col-span-2
+          className="flex items-center min-h-[100px] relative overflow-hidden
+        col-span-full md:col-[5/span_4] row-[3/span_1]"
+        >
+          <IconNext className="left-0 absolute w-full max-w-[50%] h-full z-10" />
+          <div className="left-0 bg-gradient-to-t from-black to-transparent w-full h-full absolute z-20"></div>
+          <p className="text-[calc(var(--p))] leading-6 text-[var(--color-accent-dark)] font-semibold mt-auto z-40">
+            {lang === "es" ? data_ar.list_1_2 : data_en.list_1_2}
+          </p>
+        </article>
+
+        <article
+          className="flex items-center min-h-[100px] relative overflow-hidden
+        col-span-full md:col-[9/span_4] row-[3/span_1]"
+        >
+          <IconReact className="left-0 absolute w-full max-w-[50%] h-full z-10" />
+          <div className="left-0 bg-gradient-to-t from-black to-transparent w-full h-full absolute z-20"></div>
+          <p className="text-[calc(var(--p))] leading-6 text-[var(--color-accent-dark)] font-semibold mt-auto z-40">
+            {lang === "es" ? data_ar.list_1_3 : data_en.list_1_3}
+          </p>
+        </article>
+
+        <article
+          className="flex items-center min-h-[100px] relative overflow-hidden
+        col-span-full md:col-[1/span_4] row-[4/span_1]"
+        >
+          <IconProblems className="left-0 absolute w-full max-w-[50%] h-full z-10" />
+          <div className="left-0 bg-gradient-to-t from-black to-transparent w-full h-full absolute z-20"></div>
+          <p className="text-[calc(var(--p))] leading-6 text-[var(--color-accent-dark)] font-semibold mt-auto z-40">
+            {lang === "es" ? data_ar.list_2_1 : data_en.list_2_1}
+          </p>
+        </article>
+
+        <article
+          className="flex items-center min-h-[100px] relative overflow-hidden
+        col-span-full  md:col-[5/span_4] row-[4/span_1]"
+        >
+          <IconTeam className="left-0 absolute w-full max-w-[50%] h-full z-10" />
+          <div className="left-0 bg-gradient-to-t from-black to-transparent w-full h-full absolute z-20"></div>
+          <p className="text-[calc(var(--p))] leading-6 text-[var(--color-accent-dark)] font-semibold mt-auto z-40">
+            {lang === "es" ? data_ar.list_2_2 : data_en.list_2_2}
+          </p>
+        </article>
+
+        <article
+          className="flex items-center min-h-[100px] relative overflow-hidden
+        col-span-full  md:col-[9/span_4] row-[4/span_1]"
+        >
+          <IconAuthor className="left-0 absolute w-full max-w-[50%] h-full z-10" />
+          <div className="left-0 bg-gradient-to-t from-black to-transparent w-full h-full absolute z-20"></div>
+          <p className="text-[calc(var(--p))] leading-6 text-[var(--color-accent-dark)] font-semibold mt-auto z-40">
+            {lang === "es" ? data_ar.list_2_3 : data_en.list_2_3}
+          </p>
+        </article>
+
+        {/* MARK: BLOG
+         */}
+        <article
+          className="flex flex-col gap-1
+         col-span-full md:col-[13/span_6] row-[3/span_2]
         dark:bg-gradient-to-tr dark:from-[var(--bg-primary-dark)] dark:to-[var(--bg-secondary)]"
         >
           <h2
